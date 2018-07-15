8b3a38fb982406be4c49088fc0a6e3b148b02cdc
ahmedashrafalaaser <40952658+ahmedashrafalaaser@users.noreply.github.com>
2018-07-15 15:46:22 +0200

    03.Scope Resolution Operator and Private Access Specifier  C++ Object Oriented Programming

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 #include <string>
 using namespace std;
 class human {
-public :
+private :
     string f_name;
     string l_name;
-    void welcome() {cout << "welcome "<<f_name<<" " << l_name<<endl ;}
+public :
+    void set_name (string a , string b){
+     f_name =a;
+     l_name =b;}
+void welcome() ;
 };
+ void human::welcome() {cout << "welcome "<<f_name<<" " << l_name<<endl ;}
 int main()
 {
    human a;
-   a.f_name ="ahmed";
-   a.l_name ="ashraf";
+   a.set_name("ahmed" ,"ashraf");
    a.welcome();
     return 0;
 }
