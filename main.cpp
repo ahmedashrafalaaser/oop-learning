af3af29ec25e5003262494802cd7aa53f55416ae
ahmedashrafalaaser <40952658+ahmedashrafalaaser@users.noreply.github.com>
2018-07-15 14:22:15 +0200

    02.C++ OOPS Video Tutorials for Beginners  Class Properties, Methods, Members

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
+class human {
+public :
+    string f_name;
+    string l_name;
+    void welcome() {cout << "welcome "<<f_name<<" " << l_name<<endl ;}
+};
 int main()
 {
-    cout << "Hello world!" << endl;
+   human a;
+   a.f_name ="ahmed";
+   a.l_name ="ashraf";
+   a.welcome();
     return 0;
 }
